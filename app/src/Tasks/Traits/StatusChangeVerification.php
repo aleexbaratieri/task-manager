127680195c3580ded8e0038c30928a9e809bebfb
aleexbaratieri <aleex.baratieri@gmail.com>
2025-04-12 01:16:25 -0300

    chore: change type of exception to 503 fordidden code on status change verification

--- app/src/Tasks/Traits/StatusChangeVerification.php
+++ app/src/Tasks/Traits/StatusChangeVerification.php
@@ -6,6 +6,7 @@ use Src\Tasks\Constants\TaskStatus;
 use Src\Tasks\Models\Task;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 trait StatusChangeVerification
 {
@@ -18,7 +19,7 @@ trait StatusChangeVerification
     protected function checkIfYouAreTheOwner(Task $task): void
     {
         if ($task->owner_id !== auth()->user()->id) {
-            throw new BadRequestException('You are not the owner of this task.', Response::HTTP_FORBIDDEN);
+            throw new HttpException(Response::HTTP_FORBIDDEN, 'You are not the owner of this task.');
         }
     }
 
@@ -26,14 +27,14 @@ trait StatusChangeVerification
      * Check if the current status of the task is equal to the status that needs to be changed to.
      *
      * @param string     $currentStatus The current status of the task.
-     * @param TaskStatus $needTobe      The status that needs to be changed to.
+     * @param TaskStatus $needToBe      The status that needs to be changed to.
      *
      * @throws BadRequestException
      */
-    protected function checkIfTaskStatusCanBeChanged(string $currentStatus, TaskStatus $needTobe): void
+    protected function checkIfTaskStatusCanBeChanged(string $currentStatus, TaskStatus $needToBe): void
     {
-        if ($currentStatus !== $needTobe) {
-            throw new BadRequestException('This task status cannot be changed.', Response::HTTP_FORBIDDEN);
+        if ($currentStatus !== $needToBe->value) {
+            throw new HttpException( Response::HTTP_FORBIDDEN, 'This task status cannot be changed.');
         }
     }
 }
